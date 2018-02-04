2cb7a96b206d2ce806e0f83191279efa2a6c403c
Sahana Balram <sahana2008balram@gmail.com>
2018-02-04 14:19:04 -0800

    Add button component

---
+++ src/components/Button/Button.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import './Button.css';
+
+const ButtonContainer = (props) => {
+    return (
+        <div>Button</div>
+    )
+}
+
+export default ButtonContainer;
