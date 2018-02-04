19edb4cbc66b44285e0d57179a8bb46d022cd0e6
Sahana Balram <sahana2008balram@gmail.com>
2018-02-04 14:19:23 -0800

    Add answer component

---
+++ src/components/Answers/Answers.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import './Answers.css';
+
+const AnswersContainer = (props) => {
+    return (
+        <div>Answers</div>
+    )
+}
+
+export default AnswersContainer;
