48479936d8001775f798625fe42705100db84eca
Sahana Balram <sahana2008balram@gmail.com>
2018-02-04 14:09:49 -0800

    Star component

---
+++ src/components/Stars/Stars.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import './Stars.css';
+
+const StarsContainer = (props) => {
+    return (
+        <div>...</div>
+    )
+}
+
+export default StarsContainer;
