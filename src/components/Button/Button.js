97b6e59470edfa939479cd6af4095ceaab431a43
Sahana Balram <sahana2008balram@gmail.com>
2018-02-03 17:14:36 -0800

    Intial commit

---
+++ src/components/Button/Button.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './Button.css';
+
+class ButtonContainer extends Component {
+    render() {
+        return (
+            <button>Go</button>
+        )
+    }
+}
+
+export default ButtonContainer;
