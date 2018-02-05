6362c31cded830bb2752e00cb5c671ac1c308019
Sahana Balram <sahana2008balram@gmail.com>
2018-02-05 14:10:49 -0800

    Generate random number of stars

--- src/components/Stars/Stars.js
+++ src/components/Stars/Stars.js
@@ -3,13 +3,14 @@ import {Col} from 'react-bootstrap';
 import './Stars.css';
 
 const StarsContainer = (props) => {
+    const numberOfStars = 1+ Math.floor(Math.random()*9);
+    let stars = [];
+    for(let i =0; i <numberOfStars; i++) {
+        stars.push(<i className="fa fa-star"></i>);
+    }
     return (
         <div>
-        <i className="fa fa-star"></i>
-        <i className="fa fa-star"></i>
-        <i className="fa fa-star"></i>
-        <i className="fa fa-star"></i>
-        <i className="fa fa-star"></i>
+        {stars}
         </div>
     )
 }
