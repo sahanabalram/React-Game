86b64f40d57761b8181fb8c91580f1131a537f57
Sahana Balram <sahana2008balram@gmail.com>
2018-02-05 16:22:20 -0800

    Remove container

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React, {Component} from 'react';
-import {Grid, Col, Row} from 'react-bootstrap';
 import GameContainer from './components/Game';
-import StarsContainer from './components/Stars';
-import ButtonContainer from './components/Button';
-import AnswersContainer from './components/Answers';
-import NumbersContainer from './components/Numbers';
+// import StarsContainer from './components/Stars';
+// import ButtonContainer from './components/Button';
+// import AnswersContainer from './components/Answers';
+// import NumbersContainer from './components/Numbers';
 import './App.css';
 
 class App extends Component {
@@ -12,23 +11,6 @@ class App extends Component {
     return (
       <div className="container">
         <GameContainer/>
-        <Grid>
-          <Row>
-            <Col-5>
-              <StarsContainer/>
-            </Col-5>
-
-            <Col-2>
-              <ButtonContainer/>
-            </Col-2>
-
-            <Col-2>
-              <AnswersContainer/>
-            </Col-2>
-          </Row>
-
-        </Grid>
-      <NumbersContainer/>
       </div>
     );
   }
