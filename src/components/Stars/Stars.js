e3c5bd68c795606808201c207c9dfe7b6ef9d575
Sahana Balram <sahana2008balram@gmail.com>
2018-02-05 17:00:22 -0800

    Add key value

--- src/components/Stars/Stars.js
+++ src/components/Stars/Stars.js
@@ -1,15 +1,17 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './Stars.css';
 
 const StarsContainer = (props) => {
-    const numberOfStars = 1+ Math.floor(Math.random()*9);
+    const numberOfStars = 1 + Math.floor(Math.random() * 9);
     let stars = [];
-    for(let i =0; i <numberOfStars; i++) {
-        stars.push(<i className="fa fa-star"></i>);
+    for (let i = 0; i < numberOfStars; i++) {
+        stars.push(
+            <i key={i} className="fa fa-star"></i>
+        );
     }
     return (
         <div>
-        {stars}
+            {stars}
         </div>
     )
 }
