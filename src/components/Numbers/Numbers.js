0a0403e725e29352c054ba639ae4d43e738ce991
Sahana Balram <sahana2008balram@gmail.com>
2018-02-05 16:23:14 -0800

    Dynamically create the numbers

--- src/components/Numbers/Numbers.js
+++ src/components/Numbers/Numbers.js
@@ -2,24 +2,29 @@ import React, {Component} from 'react';
 import './Numbers.css';
 
 const NumbersContainer = (props) => {
+    const numberArray = [1,2,3,4,5,6,7,8,9];
+    let numbers = [];
+    for(let i = 0; i <numberArray.length; i++) {
+        numbers.push(<span className="badge badge-pill badge-danger">{numberArray[i]}</span>)
+    }
+
+    const numberClassName = (props) => {
+        if(props.selectedNumber.indexOf(numbers) >= 0) {
+            return 'selected';
+        }
+    }
     return (
         <div className="card text-white bg-success mb-3">
             <div className="card-header">Numbers</div>
             <div className="card-body">
-            <span class="badge badge-pill badge-danger">1</span>
-            <span class="badge badge-pill badge-danger selected">2</span>
-            <span class="badge badge-pill badge-danger used">3</span>
-            <span class="badge badge-pill badge-danger">4</span>
-            <span class="badge badge-pill badge-danger">5</span>
-            <span class="badge badge-pill badge-danger">6</span>
-            <span class="badge badge-pill badge-danger">7</span>
-            <span class="badge badge-pill badge-danger">8</span>
-            <span class="badge badge-pill badge-danger">9</span>
+            {numbers.map((numbers,i) => 
+                <span  class="badge badge-pill badge-danger" onClick={() => props.selectNumber(numbers)}>
+                {numbers}
+                </span>
+            )}
             </div>
         </div>
     );
-};
-
-NumbersContainer.list = 1,10;
+}
 
 export default NumbersContainer;
